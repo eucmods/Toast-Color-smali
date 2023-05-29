34e25c6b2281e6addefe9c5f4a7327b4b33cd56c
CMODs <57895818+eucmods@users.noreply.github.com>
2023-05-29 18:40:21 -0300

    Add files via upload

---
+++ Toast Color Smali.js
@@ -0,0 +1,109 @@
+
+
+//OnCreate
+    move-object v5, v0
+
+    const-string v6, "farlight84 [Installed]"
+
+    invoke-static {v5, v6}, Lcom/cmods/MainActivity;->Color_Sucess(Landroid/content/Context;Ljava/lang/String;)V
+    
+
+
+
+
+//select smali
+.method public static Color_Sucess(Landroid/content/Context;Ljava/lang/String;)V
+    .registers 11
+    .annotation system Ldalvik/annotation/Signature;
+        value = {
+            "(",
+            "Landroid/content/Context;",
+            "Ljava/lang/String;",
+            ")V"
+        }
+    .end annotation
+    .prologue
+    .line 129
+    move-object v0, p0
+
+    move-object v1, p1
+
+    move-object v4, v0
+
+    new-instance v5, Ljava/lang/StringBuffer;
+
+    move-object v8, v5
+
+    move-object v5, v8
+
+    move-object v6, v8
+
+    invoke-direct {v6}, Ljava/lang/StringBuffer;-><init>()V
+
+    new-instance v6, Ljava/lang/StringBuffer;
+
+    move-object v8, v6
+
+    move-object v6, v8
+
+    move-object v7, v8
+
+    invoke-direct {v7}, Ljava/lang/StringBuffer;-><init>()V
+
+    const-string v7, "<font color=\'#00FF00\' ><b>"
+
+    invoke-virtual {v6, v7}, Ljava/lang/StringBuffer;->append(Ljava/lang/String;)Ljava/lang/StringBuffer;
+
+    move-result-object v6
+
+    move-object v7, v1
+
+    invoke-virtual {v6, v7}, Ljava/lang/StringBuffer;->append(Ljava/lang/String;)Ljava/lang/StringBuffer;
+
+    move-result-object v6
+
+    invoke-virtual {v6}, Ljava/lang/StringBuffer;->toString()Ljava/lang/String;
+
+    move-result-object v6
+
+    invoke-virtual {v5, v6}, Ljava/lang/StringBuffer;->append(Ljava/lang/String;)Ljava/lang/StringBuffer;
+
+    move-result-object v5
+
+    const-string v6, "</b></font>"
+
+    invoke-virtual {v5, v6}, Ljava/lang/StringBuffer;->append(Ljava/lang/String;)Ljava/lang/StringBuffer;
+
+    move-result-object v5
+
+    invoke-virtual {v5}, Ljava/lang/StringBuffer;->toString()Ljava/lang/String;
+
+    move-result-object v5
+
+    invoke-static {v5}, Landroid/text/Html;->fromHtml(Ljava/lang/String;)Landroid/text/Spanned;
+
+    move-result-object v5
+
+    const/4 v6, 0x1
+
+    invoke-static {v4, v5, v6}, Landroid/widget/Toast;->makeText(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;
+
+    move-result-object v4
+
+    invoke-virtual {v4}, Landroid/widget/Toast;->show()V
+
+    return-void
+.end method
+
+
+
+
+
+//--- [CODE BY CMODS] --- //
+
+YOURUBE
+CMODS
+@eucmods
+
+TELEGRAM
+@CoRingaModzYT
